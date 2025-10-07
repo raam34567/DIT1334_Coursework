3bf07ecb84de12530f5c6afa753f867eef722510
raam34567 <thivyaan.jr@gmail.com>
2025-10-07 20:50:48 +0800

    Add files via upload

---
+++ question2.java
@@ -0,0 +1,17 @@
+package w1_LabPractical1;
+
+import java.util.Scanner;
+
+public class question2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even.");
+        } else {
+            System.out.println(number + " is Odd.");
+        }
+        input.close();
+    }
+}
